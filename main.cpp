2c65aa7c7f9c029d9f7a5d75f55e9c184d73e0fa
FrozenLemonTee <1115306170@qq.com>
2025-10-15 23:25:45 +0800

    refactor: Add more tests for import std

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 import std;
 
+template<typename T>
+concept Addable = requires(const T& lhs, const T& rhs) {
+    { lhs + rhs } -> std::same_as<T>;
+};
+
+auto add(Addable auto const& lhs, Addable auto const& rhs) {
+    return lhs + rhs;
+}
+
 int main()
 {
-    std::println("Hello, World!");
+    std::string s = "Hello C++23 import std!\n";
+    std::string_view s_view = s;
+    std::print("{}", s_view);
+
+    std::println("1 + 1 = {}", add(1, 1));
 }
